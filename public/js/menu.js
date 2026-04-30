e775b3ce16cff8d539721180a9b9693725ae5401
Tjeerd <tj.wokke@gmail.com>
2026-04-30 15:53:54 +0200

    hamburger menu js added #12

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,1 +1,32 @@
-console.log('hamburger menu')
+
+const menu = document.querySelector('nav details')
+const body = document.querySelector('body')
+
+menu.addEventListener("toggle", function () {
+    if (menu.open) {
+        console.log('open')
+    } else {
+        console.log('close')
+    }
+
+})
+
+// buiten het menu klikken sluit het menu
+document.addEventListener("click", (event) => {
+    if (!menu.open) return
+
+    if (!menu.contains(event.target)) {
+        menu.removeAttribute('open')
+
+    }
+
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        menu.removeAttribute('open')
+    }
+
+})
+
+
