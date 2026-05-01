234b6597e451fd993aab470c95c5170368c200e0
Tjeerd <tj.wokke@gmail.com>
2026-05-01 12:06:57 +0200

    tablock and focus state updated #12

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,16 +1,27 @@
 
 const menu = document.querySelector('nav details')
 const body = document.querySelector('body')
+const content = document.querySelector('body > *:not(header)')
+const navBar = document.querySelectorAll('header ul:not(#hamburger-list)')
 
-
+// scrolllock & tablock
 menu.addEventListener("toggle", function () {
     if (menu.open) {
         body.style.overflow = 'hidden'
+        content.inert = true
+        navBar.inert = true
+        navBar.forEach(link => {
+            link.inert = true
+        });
+        menu.inert - false
     } else {
         body.style.overflow = ''
-
+        content.inert = false
+        navBar.forEach(link => {
+            link.inert = false
+        });
+        menu.inert - true
     }
-
 })
 
 // buiten het menu klikken sluit het menu
